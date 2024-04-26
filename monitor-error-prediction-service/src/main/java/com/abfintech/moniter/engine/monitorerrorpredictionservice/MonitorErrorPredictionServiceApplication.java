97884faff4d1deff92d4f2ac5d39143a5202624a
Dassapan <97041573+gokul153@users.noreply.github.com>
2024-04-26 20:28:40 +0530

    Intial_commit

---
+++ monitor-error-prediction-service/src/main/java/com/abfintech/moniter/engine/monitorerrorpredictionservice/MonitorErrorPredictionServiceApplication.java
@@ -0,0 +1,14 @@
+package com.abfintech.moniter.engine.monitorerrorpredictionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorErrorPredictionServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MonitorErrorPredictionServiceApplication.class, args);
+	}
+
+}
---
+++ monitor-error-prediction-service/src/test/java/com/abfintech/moniter/engine/monitorerrorpredictionservice/MonitorErrorPredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abfintech.moniter.engine.monitorerrorpredictionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorErrorPredictionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
